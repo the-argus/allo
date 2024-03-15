94e5ff25e9c0999d6b64268dc4ebe9b2808685f0
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-03-15 17:14:18 -0500

    add allo/status.h stuff into allo namespace

--- include/allo/status.h
+++ include/allo/status.h
@@ -4,6 +4,8 @@
 #include <ziglike/slice.h>
 #include <ziglike/status.h>
 
+namespace allo {
+
 using allocation_type_t = uint16_t;
 
 enum class AllocationStatusCode : uint8_t
@@ -40,3 +42,4 @@ using allocation_status_t = zl::status<AllocationStatusCode>;
 /// May either be a successfull allocation, or a status code failure. Check by
 /// calling okay(), and if okay, call release() to get the allocated memory.
 using allocation_result_t = zl::res<zl::slice<uint8_t>, AllocationStatusCode>;
+} // namespace allo
